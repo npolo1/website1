331e49092b93809af00e897e5f053f68906d43f2
npolo1 <93417976+npolo1@users.noreply.github.com>
2021-10-29 23:34:26 -0500

    Add files via upload

---
+++ APP.js
@@ -0,0 +1,24 @@
+
+const facebook = document.querySelector('#facebook')
+const twitter = document.querySelector('#twitter')
+const github = document.querySelector('#github')
+const proyectos = document.querySelector('#proyectos')
+const contactame = document.querySelector('#contactame')
+
+facebook.addEventListener('click', () =>{
+    window.location.href = "https://web.facebook.com/danish.alam.3194";
+})
+twitter.addEventListener('click', () =>{
+    window.location.href = "https://twitter.com/dsalam1234";
+})
+github.addEventListener('click', () =>{
+    window.location.href = "https://github.com/dantyi";
+})
+
+proyectos.addEventListener('click', () =>{
+    window.location.href = "proyectos.html";
+})
+
+contactame.addEventListener('click', () =>{
+    window.location.href = "https://wa.me/+573222279891";
+})
---
+++ APP1.js
@@ -0,0 +1,23 @@
+const facebook = document.querySelector('#facebook')
+const twitter = document.querySelector('#twitter')
+const github = document.querySelector('#github')
+const proyectos = document.querySelector('#proyectos')
+const contactame = document.querySelector('#contactame')
+
+facebook.addEventListener('click', () =>{
+    window.location.href = "https://web.facebook.com/danish.alam.3194";
+})
+twitter.addEventListener('click', () =>{
+    window.location.href = "https://twitter.com/dsalam1234";
+})
+github.addEventListener('click', () =>{
+    window.location.href = "https://github.com/dantyi";
+})
+
+proyectos.addEventListener('click', () =>{
+    window.location.href = "index.html";
+})
+
+contactame.addEventListener('click', () =>{
+    window.location.href = "https://wa.me/+573222279891";
+})
